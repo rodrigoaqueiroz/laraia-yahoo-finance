edf7f797efc119cf9f0faae27a2b4dcf70555f14
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-04-20 22:56:21 -0300

    feat: get just last 200 work days

--- main.py
+++ main.py
@@ -4,36 +4,43 @@ import pandas_datareader as web
 import streamlit as st
 import scraper
 
-stocks = ['AAPL', 'AMZN', 'INTC', 'GOOG', 'CSCO']
 
-# data= yf.download(['GOOG', 'META'], start='2021-12-10', end='2021-12-30', groupby='ticker')
-# print(data.head)
+stock = ['AAPL', 'AMZN', 'INTC', 'GOOG', 'CSCO']
+
      
 if st.sidebar.checkbox("Deseja procurar alguma ação?"):
     symbol = st.sidebar.text_input("Dígite o símbolo da ação desejada")
-    # if len(symbol) != 4:
-    #     st.error('Digite o símbolo de 4 digitos: ex.: AAPL') 
+    if len(symbol) == 4:
+      new_company_info = scraper.fetch_info(symbol)
+      stock.append(symbol)
+      if new_company_info != None:
+        scraper.fetch_default(stock)
+
+
 stock_symbol_info = st.sidebar.selectbox(
   'Informações da ação',
-  ['', *scraper.DEFAULT_COMPANIES],
+  ['', *stock],
   )
 
 stock_chart = st.sidebar.multiselect(
   'Ações para mostrar no gráfico',
-  [*scraper.DEFAULT_COMPANIES],
-  default = [*scraper.DEFAULT_COMPANIES]
+  stock,
+  default=stock
   )
-if (not stock_chart and stock_symbol_info): 
-  st.title('Informações:')
-  st.text(scraper.fetch_info(stock_symbol_info))
 
+chart_data = st.sidebar.radio(
+  'Gráfico do volume ou da cotação de fechamento ajustado', ('Volume', 'Adj Close')
+  )
 
 
 def main():
-  scraper.graph('Adj Close', stocks)
+  if len(stock_chart) > 0: 
+    scraper.render_graph(chart_data, [*stock_chart])
+  if (stock_symbol_info): 
+    st.title(f'Informações: {stock_symbol_info}')
+    st.text(scraper.fetch_info(stock_symbol_info))
 
 
 if __name__ == "__main__":
     main()
-
 
--- scraper.py
+++ scraper.py
@@ -5,52 +5,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scraper_utils_functions
 import streamlit as st
-import seaborn as sns
+from datetime import date
 
 
 DEFAULT_COMPANIES = ['AAPL', 'AMZN', 'INTC', 'GOOG', 'CSCO']
 data_content = []
 
 def fetch_default(array = DEFAULT_COMPANIES ):
-  data = yf.download(array, period='1mo')
+  data = yf.download(array, start='2021-01-01', end=date.today())
+  data = data[-200:]
   for i,v in enumerate(data):
     if i in range(0,5) or i in range(25,30):
-      # print(data[v])
       data_content.append(data[v])
-  # print(data.columns[1])
-  # return data
   df = pd.DataFrame(data_content).T
   return df
 
 
-def graph(arg, array):
-  plt.style.use('seaborn')
-  plt.figure(figsize=(12,8))
-  my_chart = st.line_chart(fetch_default()[arg][array[0]])
-  for company in array[1:]:
-    # sns.lineplot(data=fetch_default()[arg][company], x='arg' , y='company', hue="month")
-    my_chart.add_rows(fetch_default()[arg][company])
-    # st.line_chart(fetch_default()[arg][company])
-
-
 def fetch_info(symbol):
   url_profile = f'https://finance.yahoo.com/quote/{symbol}/profile?p={symbol}'
-  page = requests.get(url_profile, headers = {'User-agent': 'Mozilla/5.0'})
-  soup = BeautifulSoup(page.content, 'html.parser')
-  info = {
-    'full_name': scraper_utils_functions.get_full_name(soup),
-    'address': scraper_utils_functions.get_full_adress(soup),
-    'phone': scraper_utils_functions.get_phone(soup),
-    'sector': scraper_utils_functions.get_sector(soup),
-    'industry': scraper_utils_functions.get_industry(soup),
-    'total_employees': scraper_utils_functions.get_total_employees(soup),
-    'key_executives': scraper_utils_functions.get_key_executives(soup),
-  }
+  try:
+    page = requests.get(url_profile, headers = {'User-agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(page.content, 'html.parser')
+    info = {
+      'full_name': scraper_utils_functions.get_full_name(soup),
+      'address': scraper_utils_functions.get_full_adress(soup),
+      'phone': scraper_utils_functions.get_phone(soup),
+      'sector': scraper_utils_functions.get_sector(soup),
+      'industry': scraper_utils_functions.get_industry(soup),
+      'total_employees': scraper_utils_functions.get_total_employees(soup),
+      'key_executives': scraper_utils_functions.get_key_executives(soup),
+    }
 
-  company_info = (f"Nome: {info['full_name']}\nEndereço: {info['address']}\nPhone: {info['phone']}\nSector: {info['sector']}\nIndustry: {info['industry']}\nNúmero de funcionários: {info['total_employees']}\nPrincipais executivos: {''.join(info['key_executives']).rjust(20,'-')}")
-  
-  return company_info
+    company_info = (f"Nome: {info['full_name']}\nEndereço: {info['address']}\nPhone: {info['phone']}\nSector: {info['sector']}\nIndustry: {info['industry']}\nNúmero de funcionários: {info['total_employees']}\nPrincipais executivos: {''.join(info['key_executives']).rjust(20,'-')}")
+    return company_info
+  except:
+    print("Bad request! An exxeption ocorred")
+    return None
 
-# print(fetch_default(DEFAULT_COMPANIES))
-print(fetch_default()['Adj Close']['AMZN'])
-# print(data_content)
+
+def fetch_company_data_history(symbol):
+  data = yf.download(symbol, period='1mo')['Adj Close']
+  return data
+
+
+def render_graph(arg, array):
+  my_chart = st.line_chart(fetch_default(array)[arg][array[0]])
+  for company in array[1:]:
+    my_chart.add_rows(fetch_default(array)[arg][company])
